cde0859a5b19f343ff399054c80f556760a304e8
hyunghun.choi <parandiary@gmail.com>
2023-12-13 13:28:28 +0900

    change plugin type name

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-gem build fluent-plugin-postgres-ims.gemspec
-gem install fluent-plugin-postgres-ims-0.1.0.gem
+git pull
 
+gem build fluent-plugin-postgres-ims.gemspec
+
+gem push fluent-plugin-postgres-ims-0.1.0.gem 
+
+td-agent-gem install fluent-plugin-postgres-ims 
+
+# gem install fluent-plugin-postgres-ims-0.1.0.gem
 
 #gem push fluent-plugin-postgres-ims-0.1.0.gem
-
-
 
 systemctl restart td-agent
