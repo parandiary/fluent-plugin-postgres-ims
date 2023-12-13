8cab96cbb867dc5442ebf38c1006031d60d8ad6c
hyunghun.choi <parandiary@gmail.com>
2023-12-13 14:31:26 +0900

    readonly true test

--- install.sh
+++ install.sh
@@ -4,13 +4,22 @@ git fetch --all
 git reset --hard origin/master
 git pull
 
+echo ''
+echo ''
+echo '===============  td-agent-gem process ======================================'
+td-agent-gem build fluent-plugin-postgres-ims.gemspec
+td-agent-gem install fluent-plugin-postgres-ims-0.1.1.gem
+td-agent-gem list | grep ims
 
 
+echo ''
+echo ''
+echo '===============  gem process ======================================'
 gem build fluent-plugin-postgres-ims.gemspec
-
 gem push fluent-plugin-postgres-ims-0.1.1.gem 
 
-td-agent-gem install fluent-plugin-postgres-ims 
+
+
 
 # gem install fluent-plugin-postgres-ims-0.1.0.gem
 
--- lib/fluent/plugin/out_postgresims.rb
+++ lib/fluent/plugin/out_postgresims.rb
@@ -67,7 +67,7 @@ class Fluent::Plugin::PostgresimsOutput < Fluent::Plugin::Output
       :host => @host, :port => @port,
       :user => @username, :password => @password,
       :dbname => @database,
-      :options => "-c readOnly=false" # Set readOnly to true
+      :options => "-c readOnly=true" # Set readOnly to true
     })
   end
 
